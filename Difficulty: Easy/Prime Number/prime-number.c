fa2f1299225c01c73da582f2878fe4630ca323cb
shamrethaa <shamrethaavijayakumar@gmail.com>
2026-01-05 19:37:05 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Prime Number/prime-number.c
@@ -0,0 +1,16 @@
+bool isPrime(int n) 
+{
+    int i;
+    if (n <= 1)
+        return 0;
+
+    for (i = 2; i < n; i++)
+    {
+        if (n % i == 0)
+            return 0;
+    }
+    return 1;
+}
+
+    
+
